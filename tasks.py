d9fae79dc6c2b90ec652df12ac5e1b81641d12ff
Fotini Kiosoglou <fotinikiosoglou@macbookair.home>
2025-10-31 13:49:03 +0100

    Finished assignment

--- tasks.py
+++ tasks.py
@@ -1,21 +1,25 @@
-import numpy
+import numpy as np
 
 # Follow the tasks below to practice basic Python concepts.
 # Write your code in between the dashed lines.
 # Don't import additional packages. Numpy suffices.
 
-# Task 1: 
+# Task 1:
 # Instructions:
-#Write a function that takes one numeric argument as input. 
+#Write a function that takes one numeric argument as input.
 #If the number is larger than zero, the function should return 1, otherwise is should return -1.
 #The name of the function should be step
 
 # Your code here:
 # -----------------------------------------------
 
-def step
+def step(num):
+    if num > 0:
+        return 1
+    else:
+        return -1
 
-
+print(step(9))
 # -----------------------------------------------
 
 
@@ -28,9 +32,13 @@ def step
 
 # Your code here:
 # -----------------------------------------------
-def ReLu
+def ReLu(array, cutoff = 0):
+    array = np.array(array)
+    array[array < cutoff] = cutoff
+    return array
 
-
+test_array = np.array([-5, -1, 0, 3, 7])
+print(ReLu(test_array))
 # -----------------------------------------------
 
 
@@ -44,7 +52,19 @@ def ReLu
 # Your code here:
 # -----------------------------------------------
 
-def neural_net_layer
+def neural_net_layer(X, weights, cutoff=0):
 
+    result = np.dot(X, weights)
 
+    
+    return ReLu(result, cutoff)
+
+X = np.array([[1, 2, 3],
+              [4, 5, 6],
+              [-1, -2, -3]])
+
+weights = np.array([0.5, -1, 2])
+
+output = neural_net_layer(X, weights)
+print("Neural net layer output:", output)
 # ------------------------------------------
